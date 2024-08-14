4d849ea3185b52cfafde95db4ee691442a1ce2ee
Balazs Sevecsek <mountain@tari.com>
2024-08-14 13:22:14 +0200

    Change which key config

--- init.lua
+++ init.lua
@@ -98,7 +98,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -296,18 +296,25 @@ require('lazy').setup({
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
       require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>b'] = { name = '[B]uffers', _ = 'which_key_ignore' },
-        ['<leader>f'] = { name = '[F]ile Tree', _ = 'which_key_ignore' },
-      }
-    end,
+  
+      local wk = require("which-key")
+  
+      -- Define groups and key bindings
+      wk.add({
+        -- Group definitions
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>b', group = '[B]uffers' },
+        { '<leader>f', group = '[F]ile Tree' },
+  
+        -- Add individual mappings (if needed, examples below)
+        -- { '<leader>cf', '<cmd>SomeCommand<cr>', desc = 'Format Code' },
+        -- { '<leader>ds', '<cmd>SomeOtherCommand<cr>', desc = 'Show Documentation' },
+      })
+    end,  
   },
 
   -- NOTE: Plugins can specify dependencies.
